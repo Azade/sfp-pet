670b91a63f8ddf5ad3ded57ad93166ed50db9557
Azadeh <azadeh.win@gmail.com>
2020-11-23 23:18:26 -0800

    Adding vets page and controller. closes #7

---
+++ pet-clinic-web/src/main/java/pro/springframework/sfppetclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package pro.springframework.sfppetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "/vets/index","/vets/index.html"})
+    public String ListVets(){
+        return "vets/index";
+    }
+}
