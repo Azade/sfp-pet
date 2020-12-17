9210cdbb4a9095f9b79c849c41a37894c225898d
Azadeh <azadeh.win@gmail.com>
2020-12-17 00:35:25 -0800

    Refactored Imp to abstract ID generation. closes #23

--- pet-clinic-web/src/main/java/pro/springframework/sfppetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pro/springframework/sfppetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Helena");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Stephane");
         owner2.setLastName("Berrie");
 
@@ -38,21 +36,18 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Al");
         vet1.setLastName("Smith");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Nina");
         vet2.setLastName("Smith");
 
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
-
 
     }
 }
