adc2391f643dc00f7403ba0314441f101e04e73f
Azadeh <azadeh.win@gmail.com>
2020-12-19 14:17:27 -0800

    added PetTypeMapService closes. closes #28

---
+++ pet-clinic-data/src/main/java/pro/springframework/sfppetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package pro.springframework.sfppetclinic.services;
+
+import pro.springframework.sfppetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
