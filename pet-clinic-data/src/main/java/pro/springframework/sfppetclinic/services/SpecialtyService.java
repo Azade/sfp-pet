a1be0f7e3d0de5fc2e77f427523bf60fb0ccd2b0
Azadeh <azadeh.win@gmail.com>
2020-12-20 14:03:57 -0800

    Added Specialty Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/pro/springframework/sfppetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package pro.springframework.sfppetclinic.services;
+
+import pro.springframework.sfppetclinic.model.Speciality;
+
+public interface SpecialtyService extends CrudService<Speciality, Long>{
+}
