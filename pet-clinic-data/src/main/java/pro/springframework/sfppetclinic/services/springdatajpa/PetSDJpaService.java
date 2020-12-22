3519087e7bd92ef859074df995eb21483c30b6f4
Azadeh <azadeh.win@gmail.com>
2020-12-21 22:54:52 -0800

    implemented SDJPAService for Vets, Pets and PetTypes. closes #38

---
+++ pet-clinic-data/src/main/java/pro/springframework/sfppetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,47 @@
+package pro.springframework.sfppetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import pro.springframework.sfppetclinic.model.Pet;
+import pro.springframework.sfppetclinic.repositories.PetRepository;
+import pro.springframework.sfppetclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
